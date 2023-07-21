46bd9d751bcb79289338009e39c6e9022f5bd59d
hernan verar <hernanverar@gmail.com>
2023-07-21 11:28:47 -0700

    WIP on our First test for our Vendor an Order

--- PierreSupply/Models/Order.cs
+++ PierreSupply/Models/Order.cs
@@ -1,19 +1,25 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
+using System;
 
-// namespace PierreSupply.Models
-// {
-//   public class Order
-//   {
-//     private static List<Order> _instances = new List<Order> { };
-//     public string Name { get; set; }
-//     public int Id { get; }
-//     public List<Item> Items { get; set; }
-//   }
-//       public Order(string orderName)
-//     {
-//       Name = orderName;
-//       _instances.Add(this);
-//       Id = _instances.Count;
-//       Items = new List<Item>{};
-//     }
-// }
+
+namespace PierreSupply.Models
+{
+  public class Order
+  {
+    public string Product { get; set;}
+    public int Id { get; }
+    public string Title { get; set; }
+    private static List<Order> _instances = new List<Order> { };
+    public Order(string title, string produce)
+    {
+      Title= title;
+      Product = produce;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+      public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+  }
+}
--- PierreSupply/Models/Vendor.cs
+++ PierreSupply/Models/Vendor.cs
@@ -6,10 +6,12 @@ namespace PierreSupply.Models
   {
     private static List<Vendor> _instances = new List<Vendor> { };
     public string Name { get; set; }
+    public string Product { get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
   }
   public Vendor(string vendorName, string vendorProduct)
+
   {
     Name = vendorName;
     Product = vendorProduct;
@@ -17,4 +19,8 @@ namespace PierreSupply.Models
     Id = _instances.Count;
     Orders = new List<Order> { };
   }
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
 }
