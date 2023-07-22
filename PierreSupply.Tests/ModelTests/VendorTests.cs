5db6747db3c696a269dd0d3105e36a1b67711008
hernan verar <hernanverar@gmail.com>
2023-07-22 16:21:25 -0700

    Second passed Test to return our name back in a string

--- PierreSupply.Tests/ModelTests/VendorTests.cs
+++ PierreSupply.Tests/ModelTests/VendorTests.cs
@@ -19,5 +19,16 @@ namespace PierreSupply.Tests
       Vendor newVendor = new Vendor("test vendor", "test description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      // Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name, "test description");
+      // Act
+      string result = newVendor.Name;
+      // Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
